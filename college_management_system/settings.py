fee1f9f0a7541bc26034b109cac69f3f93ceeff2
deniza <dskarymshakova@gmail.com>
2024-12-18 16:42:42 +0600

    on render black box 1

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -145,15 +145,15 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 # #STATIC_DIRS = [os.path.join(BASE_DIR, 'main_app/static')]
-# # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 # # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
