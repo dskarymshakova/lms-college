65664ee2231b12e7accd901f563fc06b414eb611
deniza <dskarymshakova@gmail.com>
2024-12-18 12:24:11 +0600

    on render 3

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -173,6 +173,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-DISABLE_COLLECTSTATIC = 1
+#DISABLE_COLLECTSTATIC = 1
 
 
